17ce49fde135092e6fca6ad0540a0872e16b2b30
Fabio Susin <fabiosusinoberherr@gmail.com>
2019-06-10 17:19:59 -0300

    Acrescentando nova linha

---
+++ Teste(Racunho)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_Racunho_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Tetas testando!");
+            Console.WriteLine("Teste 2");
+
+        }
+    }
+}
